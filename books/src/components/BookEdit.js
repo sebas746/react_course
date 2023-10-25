4b2eb96a9cb96ddcfc395ee31808cc46dc2158cb
JUANSEBASTIAN\SEBAS <sebas746@hotmail.com>
2023-10-25 07:14:12 -0500

    Book Edit List Show.

--- books/src/components/BookEdit.js
+++ books/src/components/BookEdit.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function BookEdit({ book }) {
+function BookEdit({ book, onEdit }) {
     const [bookTitle, setBookTitle] = useState(book.title);
 
     const handleChange = (event) => {
@@ -9,7 +9,7 @@ function BookEdit({ book }) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("new title", bookTitle);
+        onEdit(book.id, bookTitle);
     };
 
     return (
