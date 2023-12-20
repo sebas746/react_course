168f05f505f15c627b4ed4d1b17b9130b1e708c1
Juan Ortegon <v-juarango@expediagroup.com>
2023-12-20 11:09:10 -0500

    Context added into the app.

--- books/src/App.js
+++ books/src/App.js
@@ -1,48 +1,20 @@
-import { useState } from "react";
+import { useEffect, useContext } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import BooksContext from "./context/books";
 
 function App() {
-    const [books, setBooks] = useState([]);
+    const { fetchBooks } = useContext(BooksContext);
 
-    const createBook = (title) => {
-        const updatedBooks = [
-            ...books,
-            { id: Math.round(Math.random() * 9999), title: title },
-        ];
-
-        setBooks(updatedBooks);
-    };
-
-    const deleteBookById = (id) => {
-        const updatedBooks = books.filter((book) => {
-            return book.id !== id;
-        });
-
-        setBooks(updatedBooks);
-    };
-
-    const editBookById = (id, newTitle) => {
-        const updatedBooks = books.map((book) => {
-            if (book.id === id) {
-                return { ...book, title: newTitle };
-            }
-
-            return book;
-        });
-
-        setBooks(updatedBooks);
-    };
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
     return (
         <div className="app">
             <h1>Reading List</h1>
-            <BookList
-                books={books}
-                onDelete={deleteBookById}
-                onEdit={editBookById}
-            />
-            <BookCreate onCreate={createBook} />
+            <BookList />
+            <BookCreate />
         </div>
     );
 }
