d33afe956b8c0ec165db25473cc8ca3c789891f3
Juan Ortegon <v-juarango@expediagroup.com>
2023-12-20 15:05:54 -0500

    Context initial exercise finished.

--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,9 +1,8 @@
+import useBooksContext from "../hooks/use-books-context";
 import BookShow from "./BookShow";
-import BooksContext from "../context/books";
-import { useContext } from "react";
 
 function BookList() {
-    const { books } = useContext(BooksContext);
+    const { books } = useBooksContext();
 
     const renderedBooks = books.map((book) => {
         return <BookShow key={book.id} book={book} />;
